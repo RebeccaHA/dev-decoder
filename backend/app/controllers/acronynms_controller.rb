a0b2fb9ed5740352896f7f313169e22fda58c1c5
RebeccaHA <rebeccahunt034@gmail.com>
2020-09-27 19:22:40 +0100

    added create, show and index actions for acronym

---
+++ backend/app/controllers/acronynms_controller.rb
@@ -0,0 +1,27 @@
+class AcronymsController < ApplicationController
+    def index
+        acronyms = Acronym.all
+
+        render json: acronyms
+    end
+
+
+    def create
+        acronym = Acronym.new(acronyms_params)
+        if acronym.save
+            render json: acronym
+        end
+    end
+
+    def show
+        acronym = Acronym.find_by(params[:id])
+
+        render json: acronym
+    end
+
+
+    private
+    def acronyms_params
+        params.require(:acronym).permit(:acronym_name, :definition, :description)
+    end
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:create]
   resources :users, only: [:create]
+  resources :acronym, only: [:create,:show,:index]
 end
