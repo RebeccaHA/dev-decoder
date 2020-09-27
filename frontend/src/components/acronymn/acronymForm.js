0cca14da0db1f8941e68d0e46c67ff722737eff4
RebeccaHA <rebeccahunt034@gmail.com>
2020-09-27 22:31:10 +0100

    added acronym form

--- frontend/src/components/acronymn/acronymForm.js
+++ frontend/src/components/acronymn/acronymForm.js
@@ -2,4 +2,17 @@ import React, { Component } from "react";
 
 function acronymForm() {
   const [text, setText] = useState("");
+
+  handleChange = () => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={this.handleChange} />
+      <input type="submit" />
+    </div>
+  );
 }
+
+export default acronymForm;
