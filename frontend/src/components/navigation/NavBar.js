be02c894b9fcddf3a8834f433c967de08a10e2a9
RebeccaHA <rebeccahunt034@gmail.com>
2020-10-01 13:25:57 +0100

    added nav styling

--- frontend/src/components/navigation/NavBar.js
+++ frontend/src/components/navigation/NavBar.js
@@ -1,18 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import HomeIcon from "@material-ui/icons/Home";
 
 const NavBar = () => {
   return (
     <div className="nav-bar">
-      <NavLink className="nav-link" to="/">
-        Home
-      </NavLink>
-      <NavLink className="nav-link" to="/acronyms">
-        Acronyms
-      </NavLink>
-      <NavLink className="nav-link" to="/new">
-        Contribute acronym
-      </NavLink>
+      <div>
+        <NavLink className="nav-link" to="/">
+          <HomeIcon />
+        </NavLink>
+      </div>
+      <div className="nav-group-right">
+        <NavLink className="nav-link" to="/acronyms">
+          Acronyms
+        </NavLink>
+        <NavLink className="nav-link" to="/new">
+          Contribute acronym
+        </NavLink>
+      </div>
     </div>
   );
 };
