38460f0dc721b74e367449b09387128b866e88ab
RebeccaHA <rebeccahunt034@gmail.com>
2020-10-05 20:31:34 +0100

    added combine reducers to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import { acronymReducer } from "./redux/acronymReducer";
+import { AuthReducer } from "./redux/AuthReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
 
+const rootReducer = combineReducers({
+  acronyms: acronymReducer,
+  users: AuthReducer
+});
 const store = createStore(
-  acronymReducer,
+  rootReducer,
   compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
